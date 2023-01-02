e35b0d5fa03ff76bfd5d7038b7b80de4c8602c9c
Mohamed <mo.esmael2019@gmail.com>
2023-01-02 11:05:56 +0200

    f1-1

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Imports\ProductImport;
+use App\Models\AddStockLine;
 use App\Models\Brand;
 use App\Models\Extension;
 use App\Models\Printer;
@@ -247,12 +248,12 @@ class ProductController extends Controller
                 })
                 ->editColumn('batch_number', '{{$batch_number}}')
                 ->editColumn('default_sell_price', function ($row) {
-                    $price=$row->variations->where('id',$row->variation_id)->first()->stockLines->where('quantity',">",'quantity_sold')->first();
+                    $price= AddStockLine::where('variation_id',$row->variation_id)->where('quantity',">",'quantity_sold')->first();
                     $price= $price? ($price->sell_price > 0 ? $price->sell_price : $row->default_sell_price):$row->default_sell_price;
                     return $this->productUtil->num_f($price);
                 })//, '{{@num_format($default_sell_price)}}')
                 ->editColumn('default_purchase_price', function ($row) {
-                    $price=$row->variations->where('id',$row->variation_id)->first()->stockLines->where('quantity',">",'quantity_sold')->first();
+                    $price= AddStockLine::where('variation_id',$row->variation_id)->where('quantity',">",'quantity_sold')->first();
                     $price= $price? ($price->purchase_price > 0 ? $price->purchase_price : $row->default_purchase_price):$row->default_purchase_price;
 
                     return $this->productUtil->num_f($price);
