6e46f28f5dd43864e3fb9de8f3a5991454b3c0bd
Mohamed <mo.esmael2019@gmail.com>
2023-01-02 11:14:51 +0200

    f1-2

--- resources/views/sale_pos/partials/filtered_products.blade.php
+++ resources/views/sale_pos/partials/filtered_products.blade.php
@@ -1,6 +1,15 @@
 @forelse ($products->chunk(5) as $chunk)
     <tr style="font-size: 11px; padding: 5px;">
         @foreach ($chunk as $product)
+            @php
+                $Variation=\App\Models\Variation::where('id',$product->variation_id)->first();
+                    if($Variation){
+                        $stockLines=$Variation->stockLines()->where('quantity',">",'quantity_sold')->first();
+                        $default_sell_price=$stockLines?$stockLines->sell_price : $Variation->default_sell_price;
+
+                    }
+
+            @endphp
             <td style="padding-top: 0px; padding-bottom: 0px;" class="product-img sound-btn filter_product_add"
                 data-is_service="{{ $product->is_service }}"
                 data-qty_available="{{ $product->qty_available - $product->block_qty }}"
@@ -14,11 +23,11 @@
                     <br> <span style="color: black; font-weight: bold;">{{ $product->sub_sku }}</span> <br>
                     @if (!empty($currency))
                         <span
-                            style="font-size:12px !important; font-weight: bold; color: black;">{{ @num_format($product->sell_price / $exchange_rate) . ' ' . $currency->symbol }}
+                            style="font-size:12px !important; font-weight: bold; color: black;">{{ @num_format($default_sell_price / $exchange_rate) . ' ' . $currency->symbol }}
                         </span>
                     @else
                         <span
-                            style="font-size:12px !important; font-weight: bold; color: black;">{{ @num_format($product->sell_price / $exchange_rate) . ' ' . session('currency.symbol') }}
+                            style="font-size:12px !important; font-weight: bold; color: black;">{{ @num_format($default_sell_price / $exchange_rate) . ' ' . session('currency.symbol') }}
                         </span>
                     @endif
                 </p>
