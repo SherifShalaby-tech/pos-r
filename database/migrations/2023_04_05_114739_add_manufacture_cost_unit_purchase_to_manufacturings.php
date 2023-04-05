22929297e972a3c409c03c7450b5cf5de7ee3fc8
doaabakhiet <doaabakhiet11@gmail.com>
2023-04-05 15:55:51 -0700

    edit

--- database/migrations/2023_04_05_114739_add_manufacture_cost_unit_purchase_to_manufacturings.php
+++ database/migrations/2023_04_05_114739_add_manufacture_cost_unit_purchase_to_manufacturings.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddManufactureCostUnitToManufacturings extends Migration
+class AddManufactureCostUnitPurchaseToManufacturings extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddManufactureCostUnitToManufacturings extends Migration
     public function up()
     {
         Schema::table('manufacturings', function (Blueprint $table) {
-            $table->decimal('manufacture_cost_unit', 15, 4)->default(0);
+            $table->decimal('manufacture_cost_unit_purchase', 15, 4)->default(0);
         });
     }
 
@@ -26,7 +26,7 @@ class AddManufactureCostUnitToManufacturings extends Migration
     public function down()
     {
         Schema::table('manufacturings', function (Blueprint $table) {
-            $table->dropColumn('manufacture_cost_unit');
+            $table->dropColumn('manufacture_cost_unit_purchase');
         });
     }
 }
