b532b977494e770ff4a60aef5e8437cfe92af7c1
doaabakhiet <doaabakhiet11@gmail.com>
2023-07-31 01:53:09 -0700

    edits

--- resources/lang/en/lang.php
+++ resources/lang/en/lang.php
@@ -1458,4 +1458,5 @@ return [
     'available' => 'Available',
     'not_available' => 'Not Available',
     'monthly_sale_and_purchase_report' => 'Monthly Sale And Purchase Report',
+    'selling_price_for_this_product_is_changed'=>'Selling Price For This Product Is Changed',
 ];
--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -853,7 +853,7 @@
                             <div class="row">
                                 <div class="col-md-4 mt-5">
                                     <div class="form-group">
-                                        <input type="button" id="submit-btn" value="@lang('lang.submit')"
+                                        <input type="button" id="submit-btn" value="{{ trans('lang.save') }}"
                                             class="btn btn-primary">
                                     </div>
                                 </div>
@@ -995,7 +995,7 @@
                             if (response.success) {
                                 swal("Success", response.msg, "success");
                                 setTimeout(() => {
-                                    window.reload();
+                                    window.close();
                                 }, 1000);
                             }
                         },
