b29d4197f74024a90d0449e9ce86ac9c9a0a6595
nour-alshikh <nooralshikh30@gmail.com>
2024-11-18 17:53:59 +0200

    pos

--- resources/views/sale_pos/partials/product_row.blade.php
+++ resources/views/sale_pos/partials/product_row.blade.php
@@ -201,7 +201,7 @@
         @elseif(env('IS_SUB_BRANCH',false)) readonly @endif
         value="@if(isset($default_sell_price)){{@num_format(($default_sell_price) / $exchange_rate)}}@else{{0}}@endif">
     </td>
-    <td style="width: @if(session('system_mode')  != 'restaurant') 2% @else 2% @endif">
+    <td style="width: @if(session('system_mode')  != 'restaurant') 6% @else 6% @endif">
         @if($product_extensions > 0)
         <span class="input-group-btn">
             <button type="button" class="btn btn-success btn-xs plus add-extension" id="add_extension">
