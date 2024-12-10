e7c57bab7089f18a5adedc0ba77aa8eb2356a444
nour-alshikh <nooralshikh30@gmail.com>
2024-12-10 15:48:37 +0200

    fix

--- public/js/pos.js
+++ public/js/pos.js
@@ -2609,8 +2609,8 @@ function get_recent_transactions() {
                 { data: "customer_name", name: "customers.name" },
                 { data: "mobile_number", name: "customers.mobile_number" },
 
-                { data: "method", name: "transaction_payments.method" },
-                { data: "ref_number", name: "transaction_payments.ref_number" },
+                // { data: "method", name: "transaction_payments.method" },
+                // { data: "ref_number", name: "transaction_payments.ref_number" },
 
                 { data: "status", name: "transactions.status" },
                 { data: "payment_status", name: "transactions.payment_status" },
