5e46529f193bc4994b4536213369a42296b6d93f
Mohammed Ismail <83010106+De-Mohamedesmael@users.noreply.github.com>
2025-04-03 01:38:28 +0200

    Update fix filter error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -346,7 +346,9 @@
     </script>
     @yield('javascript')
     @stack('javascripts')
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+</script>
     <script type="text/javascript">
         @if (session('status'))
             swal(
